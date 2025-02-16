35a1e464bca7c5a8a3186f30dd87b27e4feecf31
Manasa M <87278111+manasa-26@users.noreply.github.com>
2025-02-16 21:29:57 +0530

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,6 @@
+def preprocess_files(categorized_files):
+    """
+    Simulates any preprocessing steps.
+    """
+    print("[INFO] Preprocessing complete. Valid files are ready for scanning.")
+    return categorized_files
